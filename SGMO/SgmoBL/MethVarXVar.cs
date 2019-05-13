fa8d83779de009069971a9c854160d3344159219
sokolov-o <48639231+sokolov-o@users.noreply.github.com>
2019-05-13 20:05:08 +1000

    Update MethVarXVar.cs

--- SGMO/SgmoBL/MethVarXVar.cs
+++ SGMO/SgmoBL/MethVarXVar.cs
@@ -7,32 +7,54 @@ using System.Threading.Tasks;
 namespace SOV.SGMO
 {
     /// <summary>
-    /// Соответствие методов и переменных БД Амур переменным других (внешних, ext) баз данных, методам и переменным
+    /// Соответствие методов и переменных БД Амур переменным других (EXTernal) баз данных
     /// </summary>
     public class MethVarXVar
     {
+        /// <summary>
+        /// Словарь соответствия переменных БД Амур переменным внешних баз.
+        /// </summary>
         static Dictionary<MethVaroff, object> _MethVarXVar = new Dictionary<MethVaroff, object>()
         {
-            {
-                new MethVaroff()
-                { 
-                    MethodId=105, 
-                    Varoff=new Varoff(){ VariableId=1028, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0}
-                }, 
-                0 /*VAN-file column index*/
-            }
+            // FILE_VAN2011
+            // Dictionary value is VanRepository.WaveParamNames2011 index
+
+            // Волна смеш. - высота прогн, м
+            { new MethVaroff() { MethodId=105, Varoff=new Varoff(){ VariableId=1028, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0} }, 0},
+            // Волна ветровая - высота прогн, м
+            { new MethVaroff() { MethodId=105, Varoff=new Varoff(){ VariableId=1033, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0} }, 4},
+            // Волна ветровая - ср. период прогн, сек
+            { new MethVaroff() { MethodId=105, Varoff=new Varoff(){ VariableId=1026, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0} }, 5},
+            // Волна ветровая - направление прогн, град
+            { new MethVaroff() { MethodId=105, Varoff=new Varoff(){ VariableId=1032, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0} }, 7},
+            // Волна зыбь - высота прогн, м
+            { new MethVaroff() { MethodId=105, Varoff=new Varoff(){ VariableId=1027, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0} }, 8},
+            // Волна зыбь - ср. период прогн, сек
+            { new MethVaroff() { MethodId=105, Varoff=new Varoff(){ VariableId=1049, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0} }, 9},
+            // Волна зыбь - направление прогн, град
+            { new MethVaroff() { MethodId=105, Varoff=new Varoff(){ VariableId=1050, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0} }, 11},
+            /*
+            // НЕИЗВЕСТНО СООТВЕТСТВИЕ
+            // Волна - период пика, прогн, сек
+            { new MethVaroff() { MethodId=105, Varoff=new Varoff(){ VariableId=1051, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0} }, -1},
+            // Волна - направление доминир. прогн, град
+            { new MethVaroff() { MethodId=105, Varoff=new Varoff(){ VariableId=1035, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0} }, -1},
+            // Волна смеш. - высота макс 0.1% прогн, м
+            { new MethVaroff() { MethodId=105, Varoff=new Varoff(){ VariableId=1048, OffsetTypeId = (int)Amur.Meta.EnumOffsetType.MSL, OffsetValue=0} }, -1}
+            */
         };
+
         public static object GetExtMethodVar(int amurMethodId, Varoff amurVaroff)
         {
             MethVaroff mv = new MethVaroff() { MethodId = amurMethodId, Varoff = amurVaroff };
-            object ret;
-            _MethVarXVar.TryGetValue(mv, out ret);
+            _MethVarXVar.TryGetValue(mv, out object ret);
             return ret;
         }
         public static object GetExtMethodVar(Amur.Meta.Catalog catalog)
         {
             return GetExtMethodVar(catalog.MethodId, Varoff.GetVaroff(catalog));
         }
+
         class MethVaroff : IEquatable<MethVaroff>
         {
             public int MethodId { get; set; }
