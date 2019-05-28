3255f84928cceea69d21890bae927c734aca6932
sokolov-o <48639231+sokolov-o@users.noreply.github.com>
2019-05-28 19:16:38 +1000

    11
    1

--- Amur-2.2/Meta/CategoryDefinition.cs
+++ Amur-2.2/Meta/CategoryDefinition.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
-using FERHRI.Common;
+using SOV.Common;
 
-namespace FERHRI.Amur.Meta
+namespace SOV.Amur.Meta
 {
     /// <summary>
     /// Определение элемента категории данных.
@@ -24,28 +24,5 @@ namespace FERHRI.Amur.Meta
         public double? Value1 { get; set; }
         [DataMember]
         public double? Value2 { get; set; }
-    }
-    /// <summary>
-    /// Класс используется для отображения в формах.
-    /// </summary>
-    [DataContract]
-    public class CategoryItemLocalized
-    {
-        [DataMember]
-        public CategoryDefinition CategoryDefinition { get; set; }
-        [DataMember]
-        public string ItemName { get; set; }
-        [DataMember]
-        public string ItemNameShort { get; set; }
-        [DataMember]
-        public string ItemDescription { get; set; }
-
-        // Для отображения в формах, например, в DataGridView, нужны свойства
-        [DataMember]
-        public int Code { get { return CategoryDefinition.Code; } set { CategoryDefinition.Code = value; } }
-        [DataMember]
-        public double? Value1 { get { return CategoryDefinition.Value1; } set { CategoryDefinition.Value1 = value; } }
-        [DataMember]
-        public double? Value2 { get { return CategoryDefinition.Value2; } set { CategoryDefinition.Value2 = value; } }
     }
 }
