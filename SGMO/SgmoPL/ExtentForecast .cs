f02d3313d3a25cf0ba5a30b99da1821c2416d479
sokolov-o <48639231+sokolov-o@users.noreply.github.com>
2019-06-25 20:39:43 +1000

    1
    1

--- SGMO/SgmoPL/ExtentForecast .cs
+++ SGMO/SgmoPL/ExtentForecast .cs
@@ -22,93 +22,95 @@ namespace SOV.SGMO
         /// <returns></returns>
         public static List<DataFcs> Get(User user, List<int> extentSiteIds, DateTime dateIniUTC, int methodId)
         {
-            AmurServiceClient amurClient = new AmurServiceClient(user);
+            throw new NotImplementedException();
 
-            // GET CATALOGS, METHODS, UTCOFFSET
+            ////AmurServiceClient amurClient = new AmurServiceClient(user);
 
-            GeoRectangle[] geoRects = new GeoRectangle[]
-            {
-                                new GeoRectangle()
-                                {
-                                    NorthWest = new _TestWCFServiceField.FieldServiceReference.GeoPoint() { LatGrd = 60, LonGrd = 110 },
-                                    SouthEast = new _TestWCFServiceField.FieldServiceReference.GeoPoint() { LatGrd = 30, LonGrd = 130 }
-                                }
-                };
-            List<Catalog> catalogs = amurClient.client.GetCatalogList(amurClient.h,
-                new List<int>() { 10344 },  // Земной шар
-                null,                       // All variables
-                new List<int>() { methodId },    // f.e. GFS
-                null,                       // All sources
-                null,                       // All offset types
-                null                        // All offset values
-                );
-            FieldServiceReference.Method method = clientF.GetMethods(hf).FirstOrDefault(x => x.Id == catalogs[0].MethodId);
-            if (method == null)
-                throw new Exception(string.Format($"Запрошенный метод с кодом {} не обслуживается сервисом."));
+            ////// GET CATALOGS, METHODS, UTCOFFSET
 
-            Varoff[] varoffs = catalogs.Select(x => new Varoff() { VariableId = x.VariableId, OffsetTypeId = x.OffsetTypeId, OffsetValue = x.OffsetValue }).ToArray();
-            double[] leadTimeHours = new double[] { 0, 6 };
+            ////GeoRectangle[] geoRects = new GeoRectangle[]
+            ////{
+            ////                    new GeoRectangle()
+            ////                    {
+            ////                        NorthWest = new _TestWCFServiceField.FieldServiceReference.GeoPoint() { LatGrd = 60, LonGrd = 110 },
+            ////                        SouthEast = new _TestWCFServiceField.FieldServiceReference.GeoPoint() { LatGrd = 30, LonGrd = 130 }
+            ////                    }
+            ////    };
+            ////List<Catalog> catalogs = amurClient.client.GetCatalogList(amurClient.h,
+            ////    new List<int>() { 10344 },  // Земной шар
+            ////    null,                       // All variables
+            ////    new List<int>() { methodId },    // f.e. GFS
+            ////    null,                       // All sources
+            ////    null,                       // All offset types
+            ////    null                        // All offset values
+            ////    );
+            ////FieldServiceReference.Method method = clientF.GetMethods(hf).FirstOrDefault(x => x.Id == catalogs[0].MethodId);
+            ////if (method == null)
+            ////    throw new Exception(string.Format($"Запрошенный метод с кодом {} не обслуживается сервисом."));
+
+            ////Varoff[] varoffs = catalogs.Select(x => new Varoff() { VariableId = x.VariableId, OffsetTypeId = x.OffsetTypeId, OffsetValue = x.OffsetValue }).ToArray();
+            ////double[] leadTimeHours = new double[] { 0, 6 };
 
 
 
 
 
-            List<Amur.Meta.Method> methods = amurClient.client.GetMethods(amurClient.h, methodIds);
-            List<Catalog> allCatalogs = amurClient.client.GetCatalogList(amurClient.h, extentSiteIds, null, methodIds, null, null, null);
+            ////List<Amur.Meta.Method> methods = amurClient.client.GetMethods(amurClient.h, methodIds);
+            ////List<Catalog> allCatalogs = amurClient.client.GetCatalogList(amurClient.h, extentSiteIds, null, methodIds, null, null, null);
 
-            List<int> siteUTCOffsets = new List<int>();
-            for (int iSite = 0; iSite < extentSiteIds.Count; iSite++)
-            {
-                EntityAttrValue eav = amurClient.client.GetSiteAttrValue(amurClient.h, extentSiteIds[iSite], (int)EnumSiteAttrType.UTCOffset, DateTime.Today);
-                if (eav == null || string.IsNullOrEmpty(eav.Value))
-                    throw new Exception($"В БД Амур у пункта отсутствует атрибут UTCOffset (site.id = {extentSiteIds[iSite]}).");
-                siteUTCOffsets.Add(int.Parse(eav.Value));
-            }
+            ////List<int> siteUTCOffsets = new List<int>();
+            ////for (int iSite = 0; iSite < extentSiteIds.Count; iSite++)
+            ////{
+            ////    EntityAttrValue eav = amurClient.client.GetSiteAttrValue(amurClient.h, extentSiteIds[iSite], (int)EnumSiteAttrType.UTCOffset, DateTime.Today);
+            ////    if (eav == null || string.IsNullOrEmpty(eav.Value))
+            ////        throw new Exception($"В БД Амур у пункта отсутствует атрибут UTCOffset (site.id = {extentSiteIds[iSite]}).");
+            ////    siteUTCOffsets.Add(int.Parse(eav.Value));
+            ////}
 
-            List<DataFcs> ret = new List<DataFcs>();
+            ////List<DataFcs> ret = new List<DataFcs>();
 
-            // SCAN METHODS
+            ////// SCAN METHODS
 
-            FieldServiceClient fieldClient = new FieldServiceClient(user);
+            ////FieldServiceClient fieldClient = new FieldServiceClient(user);
 
-            for (int iMethod = 0; iMethod < methods.Count; iMethod++)
-            {
-                Console.WriteLine(string.Format($"AreaForecast.GET: forecast for method id={methods[iMethod].Name}..."));
+            ////for (int iMethod = 0; iMethod < methods.Count; iMethod++)
+            ////{
+            ////    Console.WriteLine(string.Format($"AreaForecast.GET: forecast for method id={methods[iMethod].Name}..."));
 
-                List<int> catalogIds = allCatalogs.FindAll(x => x.MethodId == methods[iMethod].Id).Select(x => x.Id).ToList();
-                if (catalogIds.Count == 0)
-                {
-                    Console.WriteLine(string.Format("** По данному треку для метода {0} отсутствуют записи каталога данных мобильного пункта.", methods[iMethod].Name));
-                    continue;
-                }
+            ////    List<int> catalogIds = allCatalogs.FindAll(x => x.MethodId == methods[iMethod].Id).Select(x => x.Id).ToList();
+            ////    if (catalogIds.Count == 0)
+            ////    {
+            ////        Console.WriteLine(string.Format("** По данному треку для метода {0} отсутствуют записи каталога данных мобильного пункта.", methods[iMethod].Name));
+            ////        continue;
+            ////    }
 
-                Dictionary<double/*leadTime*/, double[]/*Catalog index*/> fcsData = fieldClient.client.GetExtentForecast(fieldClient.h, dateIniUTC, null, 
-                    methods[iMethod], catalogIds.ToArray());
+            ////    Dictionary<double/*leadTime*/, double[]/*Catalog index*/> fcsData = fieldClient.client.GetExtentForecast(fieldClient.h, dateIniUTC, null, 
+            ////        methods[iMethod], catalogIds.ToArray());
 
-                if (fcsData == null) { Console.WriteLine("* Отсутствуют прогнозы..."); continue; }
+            ////    if (fcsData == null) { Console.WriteLine("* Отсутствуют прогнозы..."); continue; }
 
-                // CONVERT FORECAST DATA 2 List<DataSiteFcs> 
+            ////    // CONVERT FORECAST DATA 2 List<DataSiteFcs> 
 
-                int iPoint = 0;
-                foreach (KeyValuePair<double, double[]> kvp in fcsData)
-                {
-                    for (int iCatalog = 0; iCatalog < catalogIds.Count; iCatalog++)
-                    {
-                        int siteId = allCatalogs.FirstOrDefault(x => x.Id == catalogIds[iCatalog]).SiteId;
+            ////    int iPoint = 0;
+            ////    foreach (KeyValuePair<double, double[]> kvp in fcsData)
+            ////    {
+            ////        for (int iCatalog = 0; iCatalog < catalogIds.Count; iCatalog++)
+            ////        {
+            ////            int siteId = allCatalogs.FirstOrDefault(x => x.Id == catalogIds[iCatalog]).SiteId;
 
-                        ret.Add(new DataFcs
-                        {
-                            DateIniUTC = dateIniUTC,
-                            CatalogId = catalogIds[iCatalog],
-                            LeadTime = kvp.Key,
-                            Value = kvp.Value[iCatalog],
-                            UTCOffsetHours = siteUTCOffsets[extentSiteIds.IndexOf(siteId)]
-                        });
-                    }
-                    iPoint++;
-                }
-            }
-            return ret;
+            ////            ret.Add(new DataFcs
+            ////            {
+            ////                DateIniUTC = dateIniUTC,
+            ////                CatalogId = catalogIds[iCatalog],
+            ////                LeadTime = kvp.Key,
+            ////                Value = kvp.Value[iCatalog],
+            ////                UTCOffsetHours = siteUTCOffsets[extentSiteIds.IndexOf(siteId)]
+            ////            });
+            ////        }
+            ////        iPoint++;
+            ////    }
+            ////}
+            ////return ret;
         }
     }
 }
