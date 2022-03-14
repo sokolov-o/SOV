ce6b36efd72fe6337458cbd0b48ee886cec53ace
sokolov-o <48639231+sokolov-o@users.noreply.github.com>
2022-03-14 23:17:51 +1000

    qq

--- _EXE/WCFServiceField/WcfServiceField/Properties/Settings.Designer.cs
+++ _EXE/WCFServiceField/WcfServiceField/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SOV.WcfService.Field.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,16 +20,6 @@ namespace SOV.WcfService.Field.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=10.11.25.57;Port=5432;User Id=postgres;Password=qq;Database=sgmo;")]
-        public string SGMOConnectionString {
-            get {
-                return ((string)(this["SGMOConnectionString"]));
             }
         }
         
@@ -48,6 +38,16 @@ namespace SOV.WcfService.Field.Properties {
         public string LOG_FILE_PATH {
             get {
                 return ((string)(this["LOG_FILE_PATH"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=10.11.25.57;Port=5432;User Id=postgres;Password=qq;Database=sgmo;")]
+        public string SGMOConnectionString {
+            get {
+                return ((string)(this["SGMOConnectionString"]));
             }
         }
     }
